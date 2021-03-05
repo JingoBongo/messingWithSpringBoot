30a85636389e1bbbc33f2cf0bb671094f1445f79
JingoBongo <justfortoma1607>
2021-03-05 17:39:00 +0200

    initial

---
+++ src/main/java/firstAttempts/HelloController.java
@@ -0,0 +1,26 @@
+package firstAttempts;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@ComponentScan
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World equivalent from Spring Boot!";
+    }
+
+
+
+}
---
+++ src/test/java/firstAttemptsTests/HelloControllerTest.java
@@ -0,0 +1,33 @@
+package firstAttemptsTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes=HelloControllerTest.class)
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+
+
+    @Test
+    public void getHello() throws Exception {
+//        mvc = MockMvcBuilders.webAppContextSetup(this.wac).dispatchOptions(true).build();
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World equivalent from Spring Boot!")));
+    }
+}
